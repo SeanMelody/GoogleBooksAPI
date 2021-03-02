1e3221e215b4a8c336726556178ae1379da30599
Sean Melody <seandmelody@gmail.com>
2021-03-01 20:57:46 -0800

    comments added to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,15 @@
 const router = require("express").Router()
 
+// Test get request, sends back success
 router.get("/seanisthebest", (req, res) => {
     res.send({ msg: "success" });
 });
+
+
+// Need GET /books to get all the saved books
+
+// Need POST / books to save a book
+
+// Need DELETE / books/:id to delete using _id
 
 module.exports = router
